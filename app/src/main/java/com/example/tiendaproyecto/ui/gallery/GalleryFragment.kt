51adf5a036c26542e1bbd052dca01e33da64cd48
luilambu <luislambuley@gmail.com>
2021-08-18 15:37:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tiendaproyecto/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,36 @@
+package com.example.tiendaproyecto.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.tiendaproyecto.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
