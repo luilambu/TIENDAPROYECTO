5fa75c304b18f7ba7e9a880e1627f169076d693b
Luis Fernando Cruz <lfcruz@uninorte.edu.co>
2021-08-23 22:00:25 -0500

    Splash Screen - Luis Fernando Cruz

---
+++ app/src/main/java/com/example/tiendaproyecto/SplashScreenActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tiendaproyecto
+
+import android.animation.Animator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import com.example.tiendaproyecto.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+//        binding.splashAnimation
+        binding.splashAnimation.playAnimation()
+
+        binding.splashAnimation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                var intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+
+        })
+
+    }
+}
