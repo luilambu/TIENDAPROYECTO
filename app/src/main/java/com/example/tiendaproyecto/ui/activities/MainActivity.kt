1dff2a43497ecb6a54477748e9c4fa1bfc0ead86
Luis Fernando Cruz <ingluisfernandocruz@gmail.com>
2021-09-04 00:36:51 -0500

    Eliminar Settings del main, quitar FAB de mail sin funcionar - IngLuisF

--- app/src/main/java/com/example/tiendaproyecto/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/tiendaproyecto/ui/activities/MainActivity.kt
@@ -34,10 +34,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Acción de envío de correos todavía no habilitada", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        binding.appBarMain.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Acción de envío de correos todavía no habilitada", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
